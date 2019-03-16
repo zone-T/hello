55ce37e0b3f480c659ed7a64471f81375b8d1d30
ZoneTakumi <>
2019-03-16 14:32:45 +0900

    hello3

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello2 <code>src/App.js</code> and save to reload.
+            Hello3 <code>src/App.js</code> and save to reload.
           </p>
           <button onClick={hello}>
             hello
